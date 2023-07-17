318218e5ae26c32b25a69781365a7c092e342440
RamiSa24-meet <139688880+RamiSa24-meet@users.noreply.github.com>
2023-07-17 16:00:41 +0300

    im done with bonus but i didnt know if i had to ownload a pucture and upload it from the computer or the link from the web so it migh not work

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 function changeBackgroundColor(color){
-	document.body.style.backgroundColor = color;
+	//document.body.style.backgroundColor = color;
+	document.getElementById('redbut').style.visibility = '';
+document.getElementById('bluebut').style.visibility = '';
 
-	alert("lolo");
 }
 
+function onload(){
+
+document.getElementById('redbut').style.visibility = 'hidden';
+document.getElementById('bluebut').style.visibility = 'hidden';
 
 
+
+}
+
+function red(){
+	document.body.style.backgroundColor = 'red';
+	
+
+}
+function blue(){
+
+	document.body.style.backgroundColor = 'blue';
+
+
+}
